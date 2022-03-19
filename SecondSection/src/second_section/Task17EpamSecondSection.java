3d33c0389bd20911d770cab29e89b83d3b901f48
Alexander Shutov <Dubaicool@mail.ru>
2022-03-19 23:13:20 +0300

    commited task 17

--- SecondSection/src/second_section/Task17EpamSecondSection.java
+++ SecondSection/src/second_section/Task17EpamSecondSection.java
@@ -20,7 +20,9 @@ public class Task17EpamSecondSection {
     }
 
     private static int[] replaceElement(int[] data) {
+//            Variant 1:
 
+        int counter = 0;
         boolean isSorted = false;
         while (!isSorted) {
             isSorted = true;
@@ -30,20 +32,24 @@ public class Task17EpamSecondSection {
                     data[i] = data[i + 1];
                     data[i + 1] = temp;
                     isSorted = false;
+                    counter++;
                 }
             }
         }
 //            Variant 2 :
 //
+//        int counter = 0;
 //        for (int i = 0; i < data.length - 1; i++) {
 //            for (int j = 0; j < data.length - 1; j++) {
 //                if (data[j] > data[j + 1]) {
 //                    int temp = data[j];
 //                    data[j] = data[j + 1];
 //                    data[j + 1] = temp;
+//                    counter++;
 //                }
 //            }
 //        }
+        System.out.println("\nCount of steps which need for sort: " + counter);
         return data;
     }
 
